5551be8de16840265ce015a1956393122de39596
unknown <eugeneOduor79@gmail.com>
2023-10-04 15:28:27 +0300

    Changed my table name to animals

--- data.sql
+++ data.sql
@@ -1,5 +1,5 @@
 /* Populate database with sample data. */
-INSERT INTO vet_clinic (id, name, date_of_birth, escape_attempts, neutured, weight_kg)
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutured, weight_kg)
 VALUES
   (1, 'Agumon', '2020-02-03', 0, true, 10.23),
   (2, 'Gabumon', '2018-11-15', 2, true, 8),
--- queries.sql
+++ queries.sql
@@ -1,11 +1,11 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * FROM vet_clinic WHERE name LIKE '%mon';
-SELECT name FROM vet_clinic WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
-SELECT name FROM vet_clinic WHERE neutured = true AND escape_attempts < 3;
-SELECT date_of_birth FROM vet_clinic WHERE name IN ('Agumon', 'Pikachu');
-SELECT name, escape_attempts FROM vet_clinic WHERE weight_kg > 10.5;
-SELECT * FROM vet_clinic WHERE neutured = true;
-SELECT * FROM vet_clinic WHERE name != 'Devimon';
-SELECT * FROM vet_clinic WHERE weight_kg BETWEEN 10.4 AND 17.3;
+SELECT * FROM animals WHERE name LIKE '%mon';
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
+SELECT name FROM animals WHERE neutured = true AND escape_attempts < 3;
+SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutured = true;
+SELECT * FROM animals WHERE name != 'Gabumon';
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
