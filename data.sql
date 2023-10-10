13362d1e0c50f40fcc3e32e7dff5e357e55d9c6a
unknown <eugeneOduor79@gmail.com>
2023-10-10 22:18:41 +0300

    Create index for the query columns

--- data.sql
+++ data.sql
@@ -110,6 +110,7 @@ VALUES (1, 1, '2020-05-24'),
   (9, 2, '2020-08-03'),
   (10, 3, '2020-05-24'),
   (10, 1, '2021-01-11');
+
 INSERT INTO visits (animals_id, vets_id, date_of_visits)
 SELECT *
 FROM (
@@ -122,7 +123,11 @@ FROM (
   ) vets_ids,
   generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
 
-  
+
 insert into owners (full_name, email)
 select 'Owner ' || generate_series(1, 2500000),
   'owner_' || generate_series(1, 2500000) || '@mail.com';
+
+CREATE INDEX animals_id_index ON visits (animals_id);
+CREATE INDEX vets_id_index ON visits (vets_id);
+CREATE INDEX email_index ON owners (email);
